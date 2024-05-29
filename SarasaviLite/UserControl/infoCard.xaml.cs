78cbea64a00b04fc9782768d6c036a7f202380aa
chethana <chethanaravindu059@gmail.com>
2024-05-29 19:20:41 +0530

    dashbord

---
+++ SarasaviLite/UserControl/infoCard.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SarasaviLite.UserControls
+{
+    public partial class infoCard : UserControl
+    {
+        public infoCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(infoCard));
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(infoCard));
+
+        public int Amount
+        {
+            get { return (int)GetValue(AmountProperty); }
+            set { SetValue(AmountProperty, value); }
+        }
+
+        public static readonly DependencyProperty AmountProperty =
+            DependencyProperty.Register("Amount", typeof(int), typeof(infoCard));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(infoCard));
+    }
+}
---
+++ SarasaviLite/dashboard.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SarasaviLite
+{
+    /// <summary>
+    /// Interaction logic for dashboard.xaml
+    /// </summary>
+    public partial class dashboard : Window
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+     private void border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        bool IsMaximized = false;
+
+        private void border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1280;
+                    this.Height = 780;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
+
+    }
+}
